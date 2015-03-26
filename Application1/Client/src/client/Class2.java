339233f0301f941ba0ae0f5c6186652d0526b2a7
David.Edwards <David.Edwards@oracle.com>
2015-03-26 10:16:00 +0000

    Patch for release A

--- Application1/Client/src/client/Class2.java
+++ Application1/Client/src/client/Class2.java
@@ -1,6 +1,8 @@
 package client;
 
 public class Class2 {
+    int hg;
+    
     public Class2() {
         super();
     }
--- Application1/Client/src/client/Class6.java
+++ Application1/Client/src/client/Class6.java
@@ -1,6 +1,7 @@
 package client;
 
 public class Class6 extends Class5 {
+    int h;
     public Class6() {
         super();
     }
