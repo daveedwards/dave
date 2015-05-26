9fc1911f956765989a7716e9cfd4ea89d238e76d
David.Edwards <David.Edwards@oracle.com>
2015-05-26 12:03:51 +0100

    more new methods

--- Application1/Client/src/client/Class4.java
+++ Application1/Client/src/client/Class4.java
@@ -13,4 +13,12 @@ public class Class4 implements Interface1 {
     public Class4() {
         super();
     }
+
+    public void setNewAtt(List newAtt) {
+        this.newAtt = newAtt;
+    }
+
+    public List getNewAtt() {
+        return newAtt;
+    }
 }
